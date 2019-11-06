acdf593d625bf0a8e739f6cbd8a805989c03dec6
abc <abc@example.com>
2019-11-06 13:53:00 +0200

    registration/login

---
+++ server/app/Controllers/Http/UserController.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const User = use('App/Models/User')
+
+class UserController {
+ async login({ request, auth }) {
+  const { email, password } = await request.all()
+  const token = await auth.attempt(email, password)
+  return token
+ }
+
+ async register({ request }) {
+  const { email, password } = request.all()
+  await User.create({
+   email,
+   password,
+   userName: email
+  })
+  return this.login(...arguments)
+ }
+}
+
+module.exports = UserController

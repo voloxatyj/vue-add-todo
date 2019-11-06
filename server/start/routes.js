f700236f1ea267654ce05997d98a6e0c3e2f10c1
abc <abc@example.com>
2019-11-06 18:36:20 +0200

    destroy with exceptions

--- server/start/routes.js
+++ server/start/routes.js
@@ -19,5 +19,11 @@ const Route = use('Route')
 Route.group(() => {
   Route.post('auth/register', 'UserController.register')
   Route.post('auth/login', 'UserController.login')
+
+  Route.get('projects', 'ProjectController.index')
+    .middleware('auth')
+  Route.post('projects', 'ProjectController.create')
+    .middleware('auth')
+  Route.delete('projects/:id', 'ProjectController.destroy').middleware('auth')
 })
   .prefix('api')

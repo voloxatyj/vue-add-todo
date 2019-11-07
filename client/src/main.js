c19e2a039b9b69a594687c1853cfa577b98277e9
abc <abc@example.com>
2019-11-07 15:47:09 +0200

    Initial vue setup

---
+++ client/src/main.js
@@ -0,0 +1,18 @@
+import { sync } from 'vuex-router-sync';
+import Vuetify from 'vuetify';
+
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+Vue.config.productionTip = false;
+
+Vue.use(Vuetify);
+sync(store, router);
+
+new Vue({
+  router,
+  store,
+  render(h) { return h(App); },
+}).$mount('#app');

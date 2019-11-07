7ebbfcdc8e0b37cd0cb1e2922215a557f48ff090
abc <abc@example.com>
2019-11-07 12:37:26 +0200

    add tasks

--- server/start/routes.js
+++ server/start/routes.js
@@ -26,5 +26,10 @@ Route.group(() => {
     .middleware('auth')
   Route.delete('projects/:id', 'ProjectController.destroy').middleware('auth')
   Route.patch('projects/:id', 'ProjectController.update').middleware('auth')
+
+  Route.get('projects/:id/tasks', 'TaskController.index').middleware('auth')
+  Route.post('projects/:id/tasks', 'TaskController.create').middleware('auth')
+  Route.delete('tasks/:id', 'TaskController.destroy').middleware('auth')
+  Route.patch('tasks/:id', 'TaskController.update').middleware('auth')
 })
   .prefix('api')
